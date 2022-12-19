8aa0218e147007a914f7b5e293b79e85b7281eee
Marchi8 <luandeoliveirasoares@gmail.com>
2022-12-19 16:59:35 -0300

    fix: songs views and docs urls

--- songs/views.py
+++ songs/views.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework.generics import ListCreateAPIView
+from django.shortcuts import get_object_or_404
+from albums.models import Album
 
 
 class SongView(ListCreateAPIView):
@@ -13,4 +15,9 @@ class SongView(ListCreateAPIView):
     serializer_class = SongSerializer
 
     def perform_create(self, serializer):
-        serializer.save(album_id=self.kwargs["pk"])
+        album = get_object_or_404(Album, id=self.kwargs["pk"])
+        serializer.save(album_id=album.id)
+
+    def get_queryset(self):
+        album = get_object_or_404(Album, id=self.kwargs["pk"])
+        return self.queryset.filter(album_id=album.id)
